5de22b4b7c4f14cb7d6b1b1ed546819f04686a28
Vladimir <shadrov_vladimir@mail.ru>
2024-09-28 18:39:13 +0300

    Верстка списка товаров

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,118 @@
-import './App.css';
+import './app.scss';
 
 function App() {
 
   return (
-    <h1>Hello new react prodject</h1>);
+    <div className="container">
+      <div className="wok-list">
+        <div className="wok-item">
+          <div className="link-container">
+            <div className="image">
+              <img src="https://static.pizzasushiwok.ru/images/menu_new/517-300.jpg" alt="image" />
+              <div className="ingridients">масло растительное, грудка куриная, морковь, лук репчатый, перец болгарский, кабачки, соус "Чесночный", лапша пшеничная</div>
+            </div>
+            <h4 className="title">Удон с курицей</h4>
+          </div>
+          <div className="price-wrapper">
+            <div className="counter">
+              <div className="dec-btn">-</div>
+              <div className="count">1</div>
+              <div className="inc-btn">+</div>
+            </div>
+            <div className="price">
+              <div className="weight">320 г</div>
+              <div className="price-value">419 ₽</div>
+            </div>
+          </div>
+          <button className="to-cart-btn">в корзину</button>
+        </div>
+        <div className="wok-item">
+          <div className="link-container">
+            <div className="image">
+              <img src="https://static.pizzasushiwok.ru/images/menu_new/550-300.jpg" alt="image" />
+              <div className="ingridients">масло растительное, грудка куриная, морковь, лук репчатый, перец болгарский, рис, соус "Чесночный", кунжут</div>
+            </div>
+            <h4 className="title">Тяхан с курицей</h4>
+          </div>
+          <div className="price-wrapper">
+            <div className="counter">
+              <div className="dec-btn">-</div>
+              <div className="count">1</div>
+              <div className="inc-btn">+</div>
+            </div>
+            <div className="price">
+              <div className="weight">300 г</div>
+              <div className="price-value">339 ₽</div>
+            </div>
+          </div>
+          <button className="to-cart-btn">в корзину</button>
+        </div>
+        <div className="wok-item">
+          <div className="link-container">
+            <div className="image">
+              <img src="https://static.pizzasushiwok.ru/images/menu_new/518-300.jpg" alt="image" />
+              <div className="ingridients">масло растительное, говядина, морковь, лук репчатый, перец болгарский, кабачки, соус "Чесночный", лапша пшеничная</div>
+            </div>
+            <h4 className="title">Удон с говядиной</h4>
+          </div>
+          <div className="price-wrapper">
+            <div className="counter">
+              <div className="dec-btn">-</div>
+              <div className="count">1</div>
+              <div className="inc-btn">+</div>
+            </div>
+            <div className="price">
+              <div className="weight">320 г</div>
+              <div className="price-value">439 ₽</div>
+            </div>
+          </div>
+          <button className="to-cart-btn">в корзину</button>
+        </div>
+        <div className="wok-item">
+          <div className="link-container">
+            <div className="image">
+              <img src="https://static.pizzasushiwok.ru/images/menu_new/522-300.jpg" alt="image" />
+              <div className="ingridients">масло растительное, грудка куриная, морковь, лук репчатый, перец болгарский, кабачки, соус "Чесночный", лапша яичная</div>
+            </div>
+            <h4 className="title">Сомен с курицей</h4>
+          </div>
+          <div className="price-wrapper">
+            <div className="counter">
+              <div className="dec-btn">-</div>
+              <div className="count">1</div>
+              <div className="inc-btn">+</div>
+            </div>
+            <div className="price">
+              <div className="weight">320 г</div>
+              <div className="price-value">419 ₽</div>
+            </div>
+          </div>
+          <button className="to-cart-btn">в корзину</button>
+        </div>
+        <div className="wok-item">
+          <div className="link-container">
+            <div className="image">
+              <img src="https://static.pizzasushiwok.ru/images/menu_new/602-300.jpg" alt="image" />
+              <div className="ingridients">масло растительное, грудка куриная, морковь, лук репчатый, перец болгарский, кабачки, соус "Чесночный", лапша стеклянная</div>
+            </div>
+            <h4 className="title">Фунчоза с курицей</h4>
+          </div>
+          <div className="price-wrapper">
+            <div className="counter">
+              <div className="dec-btn">-</div>
+              <div className="count">1</div>
+              <div className="inc-btn">+</div>
+            </div>
+            <div className="price">
+              <div className="weight">320 г</div>
+              <div className="price-value">419 ₽</div>
+            </div>
+          </div>
+          <button className="to-cart-btn">в корзину</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
