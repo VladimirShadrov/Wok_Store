f1b4db7a524b1f3b3a52f29d66b817446beaab69
Vladimir <shadrov_vladimir@mail.ru>
2024-09-28 21:19:07 +0300

    Верстка меню навигации

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,17 @@ function App() {
 
   return (
     <div className="container">
+      <div className="nav-panel">
+        <nav className='nav-wrapper'>
+          <div className="nav-item active">Все</div>
+          <div className="nav-item">Гречневая</div>
+          <div className="nav-item">Пшеничная</div>
+          <div className="nav-item">Яичная</div>
+          <div className="nav-item">Тяханы</div>
+          <div className="nav-item">Фунчозы</div>
+          <div className="nav-item-active"></div>
+        </nav>
+      </div>
       <div className="wok-list">
         <div className="wok-item">
           <div className="link-container">
