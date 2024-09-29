c1043c6cdd11bc0e805ccf2e91ed61cd01393ec2
Vladimir <shadrov_vladimir@mail.ru>
2024-09-29 11:13:38 +0300

    Верстка выпадающего списка сортировки

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,21 @@ function App() {
           <div className="nav-item">Фунчозы</div>
           <div className="nav-item-active"></div>
         </nav>
+        <div className="sort">
+          <span>Сортировать по:</span>
+          <div className="ddl-container" data-active-class="active">
+            <div className="selected-value">Популярности (DESC)</div>
+            <div className="ddl-list">
+              <div className="ddl-list__item">Популярности (ASC)</div>
+              <div className="ddl-list__item active">Популярности (DESC)</div>
+              <div className="ddl-list__item">Цене (ASC)</div>
+              <div className="ddl-list__item">Цене (DESC)</div>
+              <div className="ddl-list__item">Алфавиту (ASC)</div>
+              <div className="ddl-list__item">Алфавиту (DESC)</div>
+            </div>
+          </div>
+
+        </div>
       </div>
       <div className="wok-list">
         <div className="wok-item">
