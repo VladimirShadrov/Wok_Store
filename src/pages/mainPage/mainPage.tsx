b7464b45e2245444776af9c78aa77a7d0ae53bb7
Vladimir <shadrov_vladimir@mail.ru>
2024-10-29 13:03:57 +0300

    На главную страницу добавлен заголовок с выбранной категорией

--- src/pages/mainPage/mainPage.tsx
+++ src/pages/mainPage/mainPage.tsx
@@ -1,12 +1,14 @@
 import Header from '../../components/headerComponent/header';
 import NavSortPanel from '../../components/navSortPanelComponent/navSortPanel';
 import ProductList from '../../components/productListComponent/productList';
+import styles from './mainPage.module.scss';
 
 const MainPage = () => {
   return (
     <>
       <Header />
       <NavSortPanel />
+      <h1 className={styles.mainTitle}>WOK Пшеничная</h1>
       <ProductList />
     </>
   );
