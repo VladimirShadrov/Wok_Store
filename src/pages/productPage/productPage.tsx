857738d7a1db4478bfb4a05d304b2b3556b8da40
Vladimir <shadrov_vladimir@mail.ru>
2024-11-12 12:37:17 +0300

    Для страницы карточки товара добавлены модульные стили

--- src/pages/productPage/productPage.tsx
+++ src/pages/productPage/productPage.tsx
@@ -2,6 +2,7 @@ import Counter from '../../components/counterComponent/counter';
 import Header from '../../components/headerComponent/header';
 import { useSelector } from 'react-redux';
 import type { RootState } from '../../store/store';
+import styles from './productPage.module.scss';
 
 const ProductPage = () => {
   const productData = useSelector((state: RootState) => state.productData.productData);
@@ -9,58 +10,58 @@ const ProductPage = () => {
   return (
     <>
       <Header />
-      <div className="product-card">
-        <div className="breadcrumbs">
-          <div className="crumb">Главная</div>
-          <div className="crumb">{productData.category}</div>
-          <div className="crumb">{productData.title}</div>
+      <div className={styles.productCard}>
+        <div className={styles.breadcrumbs}>
+          <div className={styles.crumb}>Главная</div>
+          <div className={styles.crumb}>{productData.category}</div>
+          <div className={styles.crumb}>{productData.title}</div>
         </div>
-        <div className="content">
-          <div className="image">
+        <div className={styles.content}>
+          <div className={styles.image}>
             <img src={productData.imgBig} alt="image" />
           </div>
-          <div className="description">
-            <h1 className="title">{productData.title}</h1>
-            <div className="description-block">
-              <h4 className="block-title">Состав:</h4>
+          <div className={styles.description}>
+            <h1 className={styles.title}>{productData.title}</h1>
+            <div className={styles.descriptionBlock}>
+              <h4 className={styles.blockTitle}>Состав:</h4>
               {productData.ingridients.map((ingridient) => (
-                <div className="ingridient" key={ingridient}>
+                <div className={styles.ingridient} key={ingridient}>
                   - {ingridient}
                 </div>
               ))}
             </div>
-            <div className="description-block">
-              <h4 className="block-title">Пищевая ценность на 100 г:</h4>
+            <div className={styles.descriptionBlock}>
+              <h4 className={styles.blockTitle}>Пищевая ценность на 100 г:</h4>
               {productData.nutritionalValue.map((item, index) => (
-                <div className="nutritional-item" key={item.text}>
-                  <div className="item-title">{item.text}</div>
-                  <div className="separator"></div>
-                  <div className="item-value">
+                <div className={styles.nutritionalItem} key={item.text}>
+                  <div className={styles.itemTitle}>{item.text}</div>
+                  <div className={styles.separator}></div>
+                  <div className={styles.itemValue}>
                     {item.value} {index === 0 ? 'ккал' : 'г'}
                   </div>
                 </div>
               ))}
             </div>
 
-            <div className="description-block">
-              <h4 className="block-title">Срок годности и условия хранения:</h4>
-              <div className="period-meals">24 часа при t° от +2°C до +6°C</div>
+            <div className={styles.descriptionBlock}>
+              <h4 className={styles.blockTitle}>Срок годности и условия хранения:</h4>
+              <div className={styles.periodMeals}>24 часа при t° от +2°C до +6°C</div>
             </div>
 
-            <div className="order-block">
-              <div className="wrapper">
-                <div className="price">
-                  <div className="weight">{productData.weight} г</div>
-                  <h3 className="price-value">{productData.price} ₽</h3>
+            <div className={styles.orderBlock}>
+              <div className={styles.wrapper}>
+                <div className={styles.price}>
+                  <div className={styles.weight}>{productData.weight} г</div>
+                  <h3 className={styles.priceValue}>{productData.price} ₽</h3>
                 </div>
                 <Counter />
               </div>
-              <button className="basket-btn">В корзину</button>
+              <button className={styles.basketBtn}>В корзину</button>
             </div>
 
-            <div className="product-presentation">
-              <h3 className="presentation-title">{productData.title}</h3>
-              <div className="presentation-text">{productData.description}</div>
+            <div className={styles.productPresentation}>
+              <h3 className={styles.presentationTitle}>{productData.title}</h3>
+              <div className={styles.presentationText}>{productData.description}</div>
             </div>
           </div>
         </div>
