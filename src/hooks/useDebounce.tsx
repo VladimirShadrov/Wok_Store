78cb13593c54c71ec44bc05fdc351fb0f80c50f3
Vladimir <shadrov_vladimir@mail.ru>
2024-11-15 18:12:55 +0300

    Исправлена ошибка типизации аргументов в кастомном хуке useDebounce

--- src/hooks/useDebounce.tsx
+++ src/hooks/useDebounce.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const useDebounce = (callback: () => void, delay: number) => {
+const useDebounce = <T extends (...args: unknown[]) => void>(callback: T, delay: number) => {
   const timerId = React.useRef(0);
 
   const debouncedCallback = React.useCallback(
-    (...args) => {
+    (...args: Parameters<T>) => {
       if (timerId.current) {
         clearTimeout(timerId.current);
       }
