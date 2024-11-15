55db9a845331ffba0b677047e35a3896fc4fc9ee
Vladimir <shadrov_vladimir@mail.ru>
2024-11-15 18:04:59 +0300

    В поле поиска добавлен debounce

--- src/components/headerSearchFieldComponent/searchField.tsx
+++ src/components/headerSearchFieldComponent/searchField.tsx
@@ -1,20 +1,47 @@
 import React from 'react';
 import styles from './searchField.module.scss';
 import { WokContext } from '../../context/wokContext';
+import useDebounce from '../../hooks/useDebounce';
 
 const SearchField = () => {
-  const { searchValue, setSearchValue } = React.useContext(WokContext);
+  const { setSearchValue } = React.useContext(WokContext);
+  const [inputValue, setInputValue] = React.useState('');
+  const inputRef = React.useRef(null);
+  const debouncedInput = useDebounce(changeSearchValue, 250);
+
+  const onClearInputBtnClick = () => {
+    setSearchValue('');
+    setInputValue('');
+    if (inputRef.current) {
+      const input = inputRef.current as HTMLInputElement;
+      input.focus();
+    }
+  };
+
+  function changeSearchValue() {
+    if (inputRef.current) {
+      const input = inputRef.current as HTMLInputElement;
+      setSearchValue(input.value);
+    }
+  }
+
+  const onInputChange = (value: string) => {
+    setInputValue(value);
+    debouncedInput();
+  };
+
   return (
     <div className={styles.searchWrapper}>
       <div className={styles.icon}></div>
       <input
-        onChange={(event) => setSearchValue(event.target.value)}
-        value={searchValue}
+        ref={inputRef}
+        onChange={(event) => onInputChange(event.target.value) /*(event) => setSearchValue(event.target.value)*/}
+        value={inputValue}
         type="text"
         className={styles.searchField}
         placeholder="Найти лапшу..."
       />
-      {searchValue && <div className={styles.clearBtn} onClick={() => setSearchValue('')}></div>}
+      {inputValue && <div className={styles.clearBtn} onClick={onClearInputBtnClick}></div>}
     </div>
   );
 };
