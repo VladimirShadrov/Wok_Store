cfa777655b79d706ffd69e368a6e2775f82d2f71
Vladimir <shadrov_vladimir@mail.ru>
2024-11-20 17:11:36 +0300

    Отображение общего количества и стоимости товара в корзине

--- src/components/headerBasketBtnComponent/basketBtn.tsx
+++ src/components/headerBasketBtnComponent/basketBtn.tsx
@@ -1,15 +1,18 @@
 import { Link } from 'react-router-dom';
 import styles from './basketBtn.module.scss';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store/store';
 
 const BasketBtn = () => {
+  const { totalCount, totalPrice } = useSelector((store: RootState) => store.cart);
   return (
     <div className={styles.basketBtnWrapper}>
       <Link to={'/basket'} className={styles.basketLink} />
-      <div className={styles.sum}>5 999 696 ₽</div>
+      <div className={styles.sum}>{totalPrice.toLocaleString()} ₽</div>
       <div className={styles.separator}></div>
       <div className={styles.valueWrapper}>
         <div className={styles.icon}></div>
-        <div className={styles.value}>369</div>
+        <div className={styles.value}>{totalCount}</div>
       </div>
     </div>
   );
