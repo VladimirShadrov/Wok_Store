2cbc89bb7d4339b14f871bc4c41490e032759862
Vladimir <shadrov_vladimir@mail.ru>
2024-11-21 10:08:58 +0300

    CSS модули для страницы пустой корзины

--- src/components/emptyCartComponent/emptyCart.tsx
+++ src/components/emptyCartComponent/emptyCart.tsx
@@ -1,6 +1,7 @@
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { setFilterCategory } from '../../store/slices/filterSlice';
+import styles from './emptyCart.module.scss';
 
 const EmptyCart = () => {
   const dispatch = useDispatch();
@@ -9,15 +10,15 @@ const EmptyCart = () => {
   };
 
   return (
-    <div className="empty">
-      <h2 className="empty-title">Ваша корзина пуста</h2>
-      <p className="empy-text">Вероятнее всего вы ничего не успели заказать. Для того, чтобы заказать вернитесь на главную страницу</p>
-      <div className="empty-img">
+    <div className={styles.empty}>
+      <h2 className={styles.emptyTitle}>Ваша корзина пуста</h2>
+      <p className={styles.emptyText}>Вероятнее всего вы ничего не успели заказать. Для того, чтобы заказать вернитесь на главную страницу</p>
+      <div className={styles.emptyImg}>
         <img src="src/assets/images/empty-bascket.svg" alt="image" />
       </div>
 
       <Link to={'/'} onClick={onMainPageLinkClick}>
-        <button className="empty-btn">Вернуться на главную</button>
+        <button className={styles.emptyBtn}>Вернуться на главную</button>
       </Link>
     </div>
   );
