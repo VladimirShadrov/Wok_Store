a538637a1def23a8bbc42134817a302b032a1c8e
Vladimir <shadrov_vladimir@mail.ru>
2024-11-21 13:28:08 +0300

    обновление состояния кнопки корзины в хедере после перезагрузки страницы

--- src/components/headerBasketBtnComponent/basketBtn.tsx
+++ src/components/headerBasketBtnComponent/basketBtn.tsx
@@ -1,10 +1,18 @@
 import { Link } from 'react-router-dom';
 import styles from './basketBtn.module.scss';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../../store/store';
+import React from 'react';
+import { setTotalPapams } from '../../store/slices/cartSlice';
 
 const BasketBtn = () => {
   const { totalCount, totalPrice } = useSelector((store: RootState) => store.cart);
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(setTotalPapams());
+  }, []);
+
   return (
     <div className={styles.basketBtnWrapper}>
       <Link to={'/basket'} className={styles.basketLink} />
